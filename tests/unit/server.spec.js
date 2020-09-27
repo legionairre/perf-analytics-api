d3250c8be500ee91c78e41429f6c59e7f843caa8
legionairre <guclu.kivanc@gmail.com>
2020-09-27 13:09:17 +0300

    Connection control for tests

--- tests/unit/server.spec.js
+++ tests/unit/server.spec.js
@@ -1,57 +1,65 @@
 const app = require('../../server');
+const mongoose = require('mongoose');
 const request = require('supertest');
 
 describe('API Endpoint Controls', () => {
 
-  it('should run / GET method with valid params', async () => {
-    const res = await request(app).get('/').query({
-      beginTime: 1600704587044,
-      endTime: 1700704587044
+  const url = 'mongodb://mongo:27017/docker-node-api-mongo';
+  const connect = mongoose.connect(url);
+
+  connect
+    .then(() => {
+      it('should run / GET method with valid params', async () => {
+        const res = await request(app).get('/').query({
+          beginTime: 1600704587044,
+          endTime: 1700704587044
+        })
+        expect(res.statusCode).toEqual(200);
+      });
+
+      it('should run / POST method with body', async () => {
+        const res = await request(app).post('/').send([ {
+          url: 'http://localhost:3002/',
+          analyticType: 'navigation',
+          time: '370.11',
+          startTime: '0.00',
+        } ])
+        expect(res.statusCode).toEqual(200);
+      });
+
+      it('should run / POST method with body and / GET posted data', async () => {
+
+        let expectedDocument = false;
+
+        await request(app).post('/').send([ {
+          url: 'http://localhost:3002/',
+          analyticType: 'navigation',
+          time: '370.11',
+          startTime: '0.00',
+        } ])
+
+        const getResponse = await request(app).get('/').query({
+          beginTime: new Date(new Date().getTime() - (30 * 60 * 1000)).getTime(),
+          endTime: new Date().getTime()
+        });
+
+        getResponse.body.forEach(doc => {
+          if (
+            doc.url === "http://localhost:3002/" &&
+            doc.analyticType === "navigation" &&
+            doc.time === "370.11" &&
+            doc.startTime === "0.00"
+          ) expectedDocument = true;
+        });
+
+        expect(expectedDocument).toEqual(true);
+      });
     })
-    expect(res.statusCode).toEqual(200);
-  });
+    .catch(err => console.log(err));
 
   it('should run / GET method without params', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(500);
-  });
-
-  it('should run / POST method with body', async () => {
-    const res = await request(app).post('/').send([ {
-      url: 'http://localhost:3002/',
-      analyticType: 'navigation',
-      time: '370.11',
-      startTime: '0.00',
-    } ])
-    expect(res.statusCode).toEqual(200);
-  });
-
-  it('should run / POST method with body and / GET posted data', async () => {
-
-    let expectedDocument = false;
-
-    await request(app).post('/').send([ {
-      url: 'http://localhost:3002/',
-      analyticType: 'navigation',
-      time: '370.11',
-      startTime: '0.00',
-    } ])
-
-    const getResponse = await request(app).get('/').query({
-      beginTime: new Date(new Date().getTime() - (30 * 60 * 1000)).getTime(),
-      endTime: new Date().getTime()
-    });
-
-    getResponse.body.forEach(doc => {
-      if (
-        doc.url === "http://localhost:3002/" &&
-        doc.analyticType === "navigation" &&
-        doc.time === "370.11" &&
-        doc.startTime === "0.00"
-      ) expectedDocument = true;
-    });
-
-    expect(expectedDocument).toEqual(true);
   });
 
   it('should run / POST method with invalid body', async () => {
